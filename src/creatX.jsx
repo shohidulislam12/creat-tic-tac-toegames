a1b58e1ca6590adf903a8012d403cd51211518c9
unknown <shohidulislamsifat2003@gmail.com>
2024-10-18 10:22:25 +0600

    complete basic games section

--- src/creatX.jsx
+++ src/creatX.jsx
@@ -13,7 +13,7 @@ export default function Board(){
     const [xIsNext, setXIsNext] = useState(true);
     const [squares,setSquare]=useState(Array(9).fill(null))
      function handleClick(i) {
-        if(squares[i]){
+        if(squares[i] ||CalculateWinner(squares)){
             return;
         }
         const nextSquares = squares.slice();
@@ -28,8 +28,20 @@ export default function Board(){
         setXIsNext(!xIsNext)
         
     }
+    const winner=CalculateWinner(squares);
+    let status;
+    if(winner){
+        status="winner :" +winner;
+    }
+    else {
+        status ="next playet :" +(xIsNext?"X":"O");
+
+    }
 return (
+    <>
+    <div className='status' >{status}</div>
     <div className='grid'>
+
        <Square onSquareClick={()=>handleClick(0)} value={squares[0]} />
        <Square onSquareClick={()=>handleClick(1)} value={squares[1]} />
        <Square onSquareClick={()=>handleClick(2)} value={squares[2]}  />
@@ -40,15 +52,28 @@ return (
        <Square onSquareClick={()=>handleClick(7)} value={squares[7]}  />
        <Square onSquareClick={()=>handleClick(8)} value={squares[8]} />
       
-9
-9       {/* <button className="square">2</button>
-9       <button className="square">3</button>
-9       <button className="square">4</button>
-9       <button className="square">5</button>
-9       <button className="square">6</button>
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button> */}
+
     </div>
+    </>
 )
+}
+function CalculateWinner(squares){
+    const lines=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+     
+    ];
+    for(let i=0;i<lines.length;i++){
+        const[a,b,c]=lines[i]
+        if(squares[a]&&squares[a]===squares[b]&&squares[a]===squares[c]){
+        return squares[a]
+        }
+    }
+    return null;
 }
